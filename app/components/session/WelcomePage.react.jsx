afc8d6fc9af1a7138f427a9af4bf86975dbf59d3
Tatsuya Kawasaki <ta2ya.xxx@gmail.com>
2015-07-05 18:35:40 +0900

    検索結果のデザイン変更

--- app/components/session/WelcomePage.react.jsx
+++ app/components/session/WelcomePage.react.jsx
@@ -12,9 +12,9 @@ var Result = React.createClass({
   render: function() {
     var post = this.props.post;
     return (
-      <div className="col-md-10 col-md-offset-1">
-        <h2><a onClick={this.onSelect}>{post.title}</a></h2>
-        <p className="lead">{post.body}</p>
+      <div className="col-md-10 col-md-offset-1 search-result">
+        <h3><a onClick={this.onSelect}>{post.title}</a></h3>
+        <p className="lead search-text"><small>{post.body}</small></p>
       </div>
     )
   }
@@ -27,27 +27,25 @@ var SearchResults = React.createClass({
     var existsResult = this.props.posts.length > 0 ? true : false;
     return existsResult ? (
 
-      <div className="jumbotron">
-        <div className="row spacer posts">
+      <div className="row spacer posts search-results">
 
-          {this.props.posts.map(function(post, index){
-            return (
+        {this.props.posts.map(function(post, index){
+          return (
 
-              <Result post={post} key={post.id} />
+            <Result post={post} key={post.id} />
 
-            );
-          })}
+          );
+        })}
 
-        </div>
       </div>
 
     ) : (
 
-      <div className="jumbotron">
-        <div className="row spacer noresult">
-          <div className="col-md-10 col-md-offset-1">
-            <p className="text-muted text-center">No Results</p>
-          </div>
+      <div className="row spacer noresult">
+        <div className="col-md-10 col-md-offset-1">
+          <p className="text-muted text-center">
+            <small>No Results</small>
+          </p>
         </div>
       </div>
 
@@ -86,19 +84,19 @@ var WelcomePage = React.createClass({
       <div className="row spacer">
 
         <div className="jumbotron">
-          <div className="row spacer">
 
+          <div className="row spacer">
             <div className="col-md-6 col-md-offset-3">
               <input type="text" ref="query" className="form-control" placeholder="Search for..."/>
               <div className="col-md-4 col-md-offset-4">
                 <button className="btn btn-primary" type="button" onClick={this._onSubmit}>Knowledge 検索</button>
               </div>
             </div>
-
           </div>
-        </div>
 
-        <SearchResults posts={this.state.posts}/>
+          <SearchResults posts={this.state.posts}/>
+
+        </div>
 
       </div>
 
